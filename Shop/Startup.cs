a70284e069cf8b70939171cea73731f3afeeb58a
Fernando Alves Loula da Silva <fernandoloula@gmail.com>
2021-06-12 22:30:57 -0300

    Configurando a nossa aplicação para a autenticação via token JWT

--- Shop/Startup.cs
+++ Shop/Startup.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace Shop
@@ -26,11 +22,36 @@ namespace Shop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            // Informando ao ASP.NET que a autenticação é via JWT
+            // Adicionamos antes o CORs para não termos problemas de cors ao executarmos em localhost
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Shop", Version = "v1" });
+            });
+
+            /** Aqui, de fato, informamos que temos uma autenticação JWT, configuração padrão já que é apenas para uma API e não várias
+             * Caso for trabalhar com IdentityServer ou ter um login distribuídi, então será preciso configurar o Challenge, Issuer e vários outros itens
+             * No AddJwtBearer, informamos que o formato do token é JWT
+             */
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            services.AddAuthentication(x => 
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })            
+            .AddJwtBearer(x => 
+            {
+                x.RequireHttpsMetadata = false;                                 // requirir https
+                x.SaveToken = true;                                             // salvar o token
+                x.TokenValidationParameters = new TokenValidationParameters()   // como valida o token
+                {
+                    ValidateIssuerSigningKey = true,                            // precisa validar a chave
+                    IssuerSigningKey = new SymmetricSecurityKey(key),           // informamos que é uma chave simétrica
+                    ValidateIssuer = false,                                     // não precisa validar o Issuer e nem o Audience, que são coisas quando estamos distribuindo a nossa aplicação, que remete a configuração do Challenge referido acima
+                    ValidateAudience = false
+                };
             });
         }
 
@@ -48,6 +69,20 @@ namespace Shop
 
             app.UseRouting();
 
+            /** Deve ter a chamada do UseAuthorization, caso não tenha, porém antes adicionamos o
+             * UseCors para não termos problemas com Cors, 
+             * permitindo todas as origens, métodos e cabeçalhos
+             */
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+            );
+
+            /** E então, depois de adicionamos o Cors, adicionamos o authentication e o authorization
+             * pois vamos ver toda a parte de Roles aqui
+             */
+            app.UseAuthentication(); 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
